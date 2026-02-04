c41f1067ee12e5ee32702ec27ce79dc7417b3f53
PensamientoComputacionalURL <ejcastillomon@correo.url.edu.gt>
2026-02-04 06:03:13 -0600

    Commit Inicial
    Se incluye la versión inicial del proyecto, la cual tendrán que modificar los estudiantes.

---
+++ Laboratorio/Program.cs
@@ -0,0 +1,9 @@
+﻿// Este es un comentario - será ignorado en tiempo de ejecución
+Console.WriteLine("==========================================");
+Console.WriteLine("Laboratorio # 03");
+// Ingresar aquí nombre y carnet del dueño del repositorio
+
+// Ingresar aquí nombre y carnet del colaborador:
+
+Console.WriteLine("==========================================");
+Console.ReadLine();
