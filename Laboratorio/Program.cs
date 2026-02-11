a174f2b91cd10349fb3cfb1ea33be523fc1956ba
xrMerida <xrmerida@correo.url.edu.gt>
2026-02-11 09:09:56 -0600

    Modificado Program.cs Mostrar nombre del dueño en pantalla

--- Laboratorio/Program.cs
+++ Laboratorio/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Este es un comentario - será ignorado en tiempo de ejecución
 Console.WriteLine("==========================================");
 Console.WriteLine("Laboratorio # 03");
+
 // Ingresar aquí nombre y carnet del dueño del repositorio
+Console.WriteLine("Xavier Mérida");
+Console.WriteLine("1166726");
 
 // Ingresar aquí nombre y carnet del colaborador:
-
 Console.WriteLine("==========================================");
 Console.ReadLine();
